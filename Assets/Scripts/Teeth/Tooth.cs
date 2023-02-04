2b15b0273020befc66772c9a2c6b96d8b7106506
Aaron <47363247+Aaron-Jeffers@users.noreply.github.com>
2023-02-04 15:35:58 +0000

    Set up tooth structure

---
+++ Assets/Scripts/Teeth/Tooth.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts.Teeth
+{
+    [RequireComponent(typeof(Collider2D)), RequireComponent(typeof(SpriteRenderer))]
+    public class Tooth : MonoBehaviour
+    {
+        
+    }
+}
