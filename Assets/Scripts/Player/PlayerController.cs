59c5d39eb945c6395806e43e77a331dca3566259
Aaron <47363247+Aaron-Jeffers@users.noreply.github.com>
2023-02-04 23:26:00 +0000

    Added enemy interaction

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -14,6 +14,7 @@ namespace Scripts.Player
         //Rigidbody
         Rigidbody rb;
 
+        //Particles
         [SerializeField]
         ParticleSystem particles;
 
---
+++ Assets/Scripts/Player/WaterGun.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using static UnityEngine.ParticleSystem;
+
+namespace Scripts.Player
+{
+    public class WaterGun : MonoBehaviour
+    {
+        ParticleSystem particles;
+        private List<ParticleCollisionEvent> collisionEvents;
+        [SerializeField]
+        private float individualParticleForce = 1f;
+
+        [SerializeField]
+        private float individualParticleDamage = 0.1f;
+
+        private void Awake()
+        {
+            particles = GetComponent<ParticleSystem>();
+            collisionEvents= new List<ParticleCollisionEvent>();
+        }
+        void OnParticleCollision(GameObject other)
+        {
+            var EnemyHealth = other.GetComponent<EnemyHealth>();
+
+            int numCollisionEvents = particles.GetCollisionEvents(other, collisionEvents);
+
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+            int i = 0;
+
+            while (i < numCollisionEvents)
+            {
+                if(EnemyHealth)
+                {
+                    EnemyHealth.DealDamage(individualParticleDamage);
+                }
+                if (rb)
+                {
+                    Vector3 force = collisionEvents[i].velocity * individualParticleForce;
+                    rb.AddForce(force);
+                }
+                i++;
+            }
+        }
+    }
+}
