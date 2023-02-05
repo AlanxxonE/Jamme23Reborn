79759df8984313e9e1ce54b14da3f6dd3c00ba89
AlanxxonE <AlanxxonE@users.noreply.github.com>
2023-02-05 00:52:30 +0000

    MenuFeatureCommit

---
+++ Assets/Scripts/Menu/LevelManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameObject volumeChecker;
+
+    private bool disableVolume = false;
+
+    public void LoadSceneByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex, LoadSceneMode.Single);
+    }
+
+    public void ManageVolume()
+    {
+        disableVolume = !disableVolume;
+
+        if(disableVolume)
+        {
+            AudioListener.volume = 0;
+
+            if(volumeChecker != null && !volumeChecker.activeSelf)
+            {
+                volumeChecker.SetActive(true);
+            }
+        }
+        else
+        {
+            AudioListener.volume = 100;
+
+            if (volumeChecker != null && volumeChecker.activeSelf)
+            {
+                volumeChecker.SetActive(false);
+            }
+        }
+    }
+
+    public void ExitApplication()
+    {
+        Application.Quit();
+    }
+}
