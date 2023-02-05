2bbfbb012cb77c68dd0d540a6bb88600750eaea7
Reorso <reorso@hotmail.it>
2023-02-05 15:31:32 +0000

    Animation (#25)
    added custom animator controller script

--- Assets/Scripts/Enemy/EnemyAnimationController.cs
+++ Assets/Scripts/Enemy/EnemyAnimationController.cs
@@ -4,15 +4,74 @@ using UnityEngine;
 
 public class EnemyAnimationController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    enum EnemyState
     {
-        
+        WALKING, HIT, EXPLODE, DIE
+    };
+
+    EnemyState enemyState = EnemyState.WALKING;
+    public Animator anim;
+    Transform playerTransform;
+
+
+    public bool Walk()
+    {
+        if(enemyState != EnemyState.EXPLODE || enemyState != EnemyState.DIE || enemyState != EnemyState.HIT)
+        {
+            anim.SetInteger("State", 0);
+            return true;
+        }
+        return false;
+    }
+    public bool Hit()
+    {
+        if (enemyState != EnemyState.EXPLODE || enemyState != EnemyState.DIE)
+        {
+            anim.SetInteger("State", 1);
+            return true;
+        }
+        return false;
+    }
+    public bool Explode()
+    {
+        if (enemyState != EnemyState.DIE)
+        {
+            anim.SetInteger("State", 2);
+            return true;
+        }
+        return false;
+    }
+    public void Die()
+    {
+            anim.SetInteger("State", 3);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
-        
+        Vector3 direction = playerTransform.position - transform.position;
+        float angle = Vector3.SignedAngle(direction, transform.forward, Vector3.up);
+
+        if (Mathf.Abs(angle) < 45f)
+        {
+            anim.SetInteger("view", 0);                 //front
+        }
+        else if (Mathf.Abs(angle) > 135)
+        {
+            anim.SetInteger("view", 1);                 //back
+        }
+        else 
+        {
+            if (angle < 0)
+            {
+                anim.SetBool("left", true);
+                anim.gameObject.GetComponent<SpriteRenderer>().flipY = true;            //left
+            }
+            else
+            {
+                anim.SetBool("left", false);
+                anim.gameObject.GetComponent<SpriteRenderer>().flipY = false;           //right
+            }
+            anim.SetInteger("view", 2);                 //side
+        }
     }
 }
